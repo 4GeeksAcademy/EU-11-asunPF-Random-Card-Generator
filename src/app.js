c9e7bfb145ae9a2fa6a6fb08a37020eb74ad851c
Asun Perea Ferrer <118168204+asun-pf@users.noreply.github.com>
2023-05-31 21:02:02 +0000

    I've created a cardGenerator function with two arrays that will be called with the id's, querySelector and Element. I have to randomize this call with my random constants and introduce my elementos to my html. in order to change color is the if statement.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+/*function restorePage () {
+  location.reload ();
+ }*/
+
+window.onload = () => {
+  cardGenerator();
 };
+
+function cardGenerator() {
+  setInterval(() => {
+    location.reload();
+  }, 10000);
+
+  const suites = ["♦", "♥", "♠", "♣"];
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+  const topSuiteElement = document.querySelector(".top-suite");
+  const numberElement = document.querySelector(".number");
+  const bottomSuiteElement = document.querySelector(".bottom-suite");
+
+  const randomSuites = suites[Math.floor(Math.random() * suites.length)];
+  const randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+
+  topSuiteElement.innerHTML = randomSuites;
+  numberElement.innerHTML = randomNumbers;
+  bottomSuiteElement.innerHTML = randomSuites;
+
+  if (randomSuites === "♥" || randomSuites === "♦") {
+    topSuiteElement.classList.add("red");
+    numberElement.classList.add("red");
+    bottomSuiteElement.classList.add("red");
+  }
+}
+
+let timeleft = 10;
+const downloadTimer = setInterval(function() {
+  timeleft--;
+  document.getElementById("countdowntimer").textContent = timeleft;
+  if (timeleft <= 0) clearInterval(downloadTimer);
+}, 1000);
